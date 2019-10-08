33cbdc2ba574644fecafd74f199de7135af87b11
Meet Chheda <meetmc96@gmail.com>
2019-10-07 21:24:12 -0700

    Updating the UI for the parse prototype DONOG-13

--- ParsePrototype/app/src/main/java/com/donogear/parseprototype/App.java
+++ ParsePrototype/app/src/main/java/com/donogear/parseprototype/App.java
@@ -3,16 +3,17 @@ package com.donogear.parseprototype;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -21,7 +22,8 @@ public class App extends Activity {
     private Timer myTimer;
     private int currentPrice = 0;
     private String itemName = "MyItem";
-
+    int secondsLeft = 16800;
+    ImageButton imageButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,26 +38,67 @@ public class App extends Activity {
                 .build()
         );
 
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Item");
+        query.whereEqualTo("name", itemName);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> items, ParseException e) {
+                TextView currentBidTextView = findViewById(R.id.currentBid);
+                TextView bidby = findViewById(R.id.bidby);
+                TextView timeLeft = findViewById(R.id.timeleft);
+                imageButton = findViewById(R.id.pricebutton);
+                if (e == null) {
+                    if (!items.isEmpty()) {
+                        currentPrice = (Integer) items.get(0).get("price");
+                        currentBidTextView.setText("$"+ String.valueOf(currentPrice));
+                        if (String.valueOf(currentPrice).length() == 3) {
+                            imageButton.setLayoutParams(new RelativeLayout.LayoutParams(160, 110));
+                        }
+                        if (String.valueOf(currentPrice).length() == 4) {
+                            imageButton.setLayoutParams(new RelativeLayout.LayoutParams(210, 110));
+                        }
+                        bidby.setText(String.valueOf(items.get(0).get("user")));
+                        float hoursLeft = (float)secondsLeft / 3600;
+                        float minsLeft = hoursLeft - (int)hoursLeft;
+                        minsLeft = minsLeft * 60;
+                        timeLeft.setText((int)hoursLeft+"h " + (int)minsLeft+"m");
+                    }
+                } else {
+                    // object does not exist
+                    currentPrice = 0;
+                }
+
+
+            }
+        });
+
         // Create a timer to check for updated price every second
         myTimer = new Timer();
         myTimer.schedule(new TimerTask() {
             @Override
             public void run() {
+                secondsLeft = secondsLeft - 1;
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("Item");
                 query.whereEqualTo("name", itemName);
                 query.findInBackground(new FindCallback<ParseObject>() {
                     public void done(List<ParseObject> items, ParseException e) {
-                        TextView currentBidTextView = (TextView)findViewById(R.id.currentBid);
+                        TextView currentBidTextView = findViewById(R.id.currentBid);
+                        TextView bidby = findViewById(R.id.bidby);
+                        TextView timeLeft = findViewById(R.id.timeleft);
                         if (e == null) {
                             if (!items.isEmpty()) {
                                 currentPrice = (Integer) items.get(0).get("price");
+                                //currentBidTextView.setText("Current Bid: " + String.valueOf(currentPrice) + " by " + items.get(0).get("user"));
+                                currentBidTextView.setText("$"+ String.valueOf(currentPrice));
+                                bidby.setText(String.valueOf(items.get(0).get("user")));
+                                float hoursLeft = (float)secondsLeft / 3600;
+                                float minsLeft = hoursLeft - (int)hoursLeft;
+                                minsLeft = minsLeft * 60;
+                                timeLeft.setText((int)hoursLeft+"h " + (int)minsLeft+"m");
                             }
                         } else {
                             // object does not exist
                             currentPrice = 0;
                         }
-
-                        currentBidTextView.setText(String.valueOf(currentPrice));
                     }
                 });
             }
@@ -100,10 +143,12 @@ public class App extends Activity {
                             item.put("price", bid);
                             item.saveInBackground();
                         } else {
-                            // Error occurred
+
                         }
                     }
                 });
+            } else {
+                Toast.makeText(getApplicationContext(), "Please bid higher", Toast.LENGTH_SHORT).show();
             }
         }
     }
