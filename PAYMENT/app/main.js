31f798bd26afb6f4efd7e969d5465d18795b7b44
Meet Chheda <meetmc96@gmail.com>
2019-10-07 17:44:16 -0700

    Connect stripe app with the backend back4app DONOG-8

---
+++ PAYMENT/app/main.js
@@ -0,0 +1,40 @@
+et stripe_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"; //Secret Key here
+let Stripe = require("stripe")(stripe_key);
+
+Parse.Cloud.define("purchaseItem", async (request) => {
+    let item, order;
+    let itemQuery = new Parse.Query('Item');
+    itemQuery.equalTo('itemName', request.params.itemName);
+
+    const result = await itemQuery.first(null,{useMasterKey: true});
+
+    if(!result){
+        throw new Error('Sorry, this item is no longer available.');
+    } else if(result.get('quantity') <= 0){
+        throw new Error('Sorry, this item is out of stock.');
+    } else {
+        item = result;
+        const charge = await Stripe.charges.create({
+            amount: item.get('price')*100, // It needs to convert to cents
+            currency: "usd",
+            source: request.params.cardToken,
+            description: "Charge for " + request.params.email
+        })
+
+        item.increment('quantity', -1);
+        const object = await item.save(null,{useMasterKey: true})
+        const order = new Parse.Object("Order");
+        order.set('name', request.params.name);
+        order.set('email', request.params.email);
+        order.set('address', request.params.address);
+        order.set('zip', request.params.zip);
+        order.set('city_state', request.params.city_state);
+        order.set('item', item.get('ItemName'));
+        order.set('fulfilled', true);
+        order.set('charged', false);
+        order.set('stripePaymentId', charge.id);
+        order.set('charged', true);
+        await order.save(null,{useMasterKey:true});
+    }
+    return 'OK';
+});
