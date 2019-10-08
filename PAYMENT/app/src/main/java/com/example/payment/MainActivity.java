bebb5492d43e864296cce621d56d562d798992a9
Devansh Gada <devanshgada@devanshs-mbp.home>
2019-10-07 18:22:02 -0700

    Adding additional features to payment protoype DONOG-9

--- PAYMENT/app/src/main/java/com/example/payment/MainActivity.java
+++ PAYMENT/app/src/main/java/com/example/payment/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.payment;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,38 +17,92 @@ import com.parse.ParseCloud;
 import com.parse.ParseException;
 import com.stripe.android.model.Card;
 import com.stripe.android.model.Token;
+import com.stripe.android.view.CardMultilineWidget;
 
 import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String PUBLISHABLE_KEY = "pk_test_BS3lFguklXYlFf8oc5Ul9kg300mSOJkSYT";
     private Card card;
+    private CardMultilineWidget cardMultilineWidget;
+    public static final String PUBLISHABLE_KEY = "pk_test_BS3lFguklXYlFf8oc5Ul9kg300mSOJkSYT";
+    public static HashMap<String, Object> params;
     private ProgressDialog progress;
-    private Button purchase;
+    private Button purchase, reset, shipping;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        card = Card.create (
-                "4242424242424242",
-                12,
-                2030,
-                "123"
-        );
+        cardMultilineWidget = findViewById(R.id.card_widget);
         progress = new ProgressDialog(this);
-        purchase = (Button) findViewById(R.id.purchase);
+
+        init();
         purchase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 buy();
             }
         });
-
+        shipping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                card = cardMultilineWidget.getCard();
+                startActivity(new Intent(getBaseContext(), ShippingActivity.class));
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardMultilineWidget.clear();
+            }
+        });
     }
+
+    public void onSaveInstanceState (Bundle savedInstancestate) {
+        super.onSaveInstanceState(savedInstancestate);
+        card = cardMultilineWidget.getCard();
+        if (card != null) {
+            savedInstancestate.putString("cardNumber", card.getNumber());
+            savedInstancestate.putInt("month", card.getExpMonth());
+            savedInstancestate.putInt("year", card.getExpYear());
+            savedInstancestate.putString("CVV", card.getCVC());
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (cardMultilineWidget.getCard() != null) {
+            cardMultilineWidget.setCardNumber(savedInstanceState.getString("cardNumber"));
+            cardMultilineWidget.setCvcCode(savedInstanceState.getString("CVV"));
+            int month = savedInstanceState.getInt("month");
+            int year = savedInstanceState.getInt("year");
+            cardMultilineWidget.setExpiryDate(month, year);
+        }
+    }
+
+    private void init() {
+        purchase = findViewById(R.id.purchase);
+        shipping = findViewById(R.id.shipping);
+        reset = findViewById(R.id.reset);
+
+        params = new HashMap<>();
+        params.put("name","Meet Chheda");
+        params.put("email","meet@gmail.com");
+        params.put("address","Los Angeles");
+        params.put("zip","90007");
+        params.put("city_state","CA");
+        params.put("ItemName", "test");
+    }
+
     private void buy(){
+        card = cardMultilineWidget.getCard();
+        if (card == null) {
+            Toast.makeText(MainActivity.this, "Enter valid details", Toast.LENGTH_SHORT).show();
+            return;
+        }
         boolean validation = card.validateCard();
         if(validation){
             startProgress("Validating Credit Card");
@@ -59,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
                                     "Stripe -" + error.toString(),
                                     Toast.LENGTH_LONG).show();
                         }
-
 
                         public void onSuccess(Token token) {
                             System.out.println("Token: " + token);
@@ -87,15 +142,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void charge(Token cardToken){
-        HashMap<String, Object> params = new HashMap<String, Object>();
-        params.put("ItemName", "test");
-
         params.put("cardToken", cardToken.getId());
-        params.put("name","Dominic Wong");
-        params.put("email","dominwong4@gmail.com");
-        params.put("address","HIHI");
-        params.put("zip","99999");
-        params.put("city_state","CA");
         startProgress("Purchasing Item");
         ParseCloud.callFunctionInBackground("purchaseItem", params, new FunctionCallback<Object>() {
             public void done(Object response, ParseException e) {
@@ -121,11 +168,10 @@ public class MainActivity extends AppCompatActivity {
         progress.setMessage("Please Wait");
         progress.show();
     }
+
     private void finishProgress(){
         progress.dismiss();
 
 
     }
-
-
 }
