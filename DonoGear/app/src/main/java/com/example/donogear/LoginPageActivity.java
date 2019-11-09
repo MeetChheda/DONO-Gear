daffb50105c3033e14327ad644d64f4390c86df2
ParthPanchmia <ppanchmi@usc.edu>
2019-11-09 15:37:59 -0800

    Added code to create account and login.

--- DonoGear/app/src/main/java/com/example/donogear/LoginPageActivity.java
+++ DonoGear/app/src/main/java/com/example/donogear/LoginPageActivity.java
@@ -4,29 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-
-import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.parse.LogInCallback;
+
+
 
 public class LoginPageActivity extends AppCompatActivity {
 
-    TabItem createAccountTab;
-    TabItem loginTab;
     TabLayout tabs;
     LinearLayout login_layout;
     LinearLayout createAccount_layout;
+    EditText usernameEditTextSignup;
+    EditText passwordEditTextSignup;
+    EditText usernameEditTextLogin;
+    EditText passwordEditTextLogin;
+    String username;
+    String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+
         tabs = findViewById(R.id.tabs);
         login_layout = findViewById(R.id.login_layout);
         createAccount_layout = findViewById(R.id.createAccount_layout);
-//        createAccountTab = findViewById(R.id.createAccount_tab);
-//        loginTab = findViewById(R.id.login_tab);
-//        loginTab.setBackgroundColor(Color.parseColor("#2FD6D6"));
+        usernameEditTextSignup = (EditText)findViewById(R.id.emailCreateAccount_editText);
+        passwordEditTextSignup = (EditText)findViewById(R.id.passwordCreateAccount_editText);
+        usernameEditTextLogin = (EditText)findViewById(R.id.email_editText);
+        passwordEditTextLogin = (EditText)findViewById(R.id.password_editText);
 
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -35,15 +62,11 @@ public class LoginPageActivity extends AppCompatActivity {
                     case 0: {
                         createAccount_layout.setVisibility(View.GONE);
                         login_layout.setVisibility(View.VISIBLE);
-//                        loginTab.setBackgroundColor(Color.parseColor("#2FD6D6"));
-//                        createAccountTab.setBackgroundColor(Color.parseColor("#FFFFFF"));
                         break;
                     }
                     case 1: {
                         login_layout.setVisibility(View.GONE);
                         createAccount_layout.setVisibility(View.VISIBLE);
-//                        createAccountTab.setBackgroundColor(Color.parseColor("#2FD6D6"));
-//                        loginTab.setBackgroundColor(Color.parseColor("#FFFFFF"));
                         break;
                     }
                 }
@@ -61,11 +84,69 @@ public class LoginPageActivity extends AppCompatActivity {
         });
     }
 
+    private void alertDisplayer(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginPageActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+//                        Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        startActivity(intent);
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+    }
+
     public void createAccountBtnClicked(View view) {
 
+        username = usernameEditTextSignup.getText().toString();
+        password = passwordEditTextSignup.getText().toString();
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(username);
+
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    alertDisplayer("Sucessful Sign Up!","Welcome" + username + "!");
+                } else {
+                    ParseUser.logOut();
+//                    Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void loginBtnClicked(View view) {
+
+
+        username = usernameEditTextLogin.getText().toString();
+        password = passwordEditTextLogin.getText().toString();
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser parseUser, ParseException e) {
+                if (parseUser != null) {
+                    alertDisplayer("Sucessful Login","Welcome back" + username + "!");
+                } else {
+                    ParseUser.logOut();
+                    Toast.makeText(LoginPageActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void logoutBtnClicked(View view) {
+
+        ParseUser.logOut();
+        alertDisplayer("So, you're going...", "Ok...Bye-bye then");
 
     }
 }
