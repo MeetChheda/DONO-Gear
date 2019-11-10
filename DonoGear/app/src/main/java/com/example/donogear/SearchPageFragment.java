12f698e9c4d0aec55b117d8a4b31f9d7d2a4dfa5
Meet Chheda <meetmc96@gmail.com>
2019-11-09 22:23:55 -0800

    UI Change DONOG-88

--- DonoGear/app/src/main/java/com/example/donogear/SearchPageFragment.java
+++ DonoGear/app/src/main/java/com/example/donogear/SearchPageFragment.java
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,12 +20,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
