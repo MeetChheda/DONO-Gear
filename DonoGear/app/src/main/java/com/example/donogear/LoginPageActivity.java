947b70d5b8cc9fa62205d1e0b52d43303b8ba8d9
FS1596 <46803272+FS1596@users.noreply.github.com>
2019-11-10 18:10:43 -0800

    login done with validation

--- DonoGear/app/src/main/java/com/example/donogear/LoginPageActivity.java
+++ DonoGear/app/src/main/java/com/example/donogear/LoginPageActivity.java
@@ -21,6 +21,8 @@ import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.parse.LogInCallback;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class LoginPageActivity extends AppCompatActivity {
@@ -32,8 +34,10 @@ public class LoginPageActivity extends AppCompatActivity {
     EditText passwordEditTextSignup;
     EditText usernameEditTextLogin;
     EditText passwordEditTextLogin;
+    EditText emailEditTextSignUp;
     String username;
     String password;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +51,14 @@ public class LoginPageActivity extends AppCompatActivity {
                 .build()
         );
 
+//        boolean finish = getIntent().getBooleanExtra("finish", false);
         tabs = findViewById(R.id.tabs);
         login_layout = findViewById(R.id.login_layout);
         createAccount_layout = findViewById(R.id.createAccount_layout);
-        usernameEditTextSignup = (EditText)findViewById(R.id.emailCreateAccount_editText);
+        usernameEditTextSignup = (EditText) findViewById(R.id.usernameCreateAccount_editText);
+        emailEditTextSignUp = (EditText)findViewById(R.id.emailCreateAccount_editText);
         passwordEditTextSignup = (EditText)findViewById(R.id.passwordCreateAccount_editText);
-        usernameEditTextLogin = (EditText)findViewById(R.id.email_editText);
+        usernameEditTextLogin = (EditText)findViewById(R.id.username_editText);
         passwordEditTextLogin = (EditText)findViewById(R.id.password_editText);
 
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -84,57 +90,93 @@ public class LoginPageActivity extends AppCompatActivity {
         });
     }
 
-    private void alertDisplayer(String title,String message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(LoginPageActivity.this)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-//                        Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                        startActivity(intent);
-                    }
-                });
-        AlertDialog ok = builder.create();
-        ok.show();
-    }
+//    private void alertDisplayer(String title,String message){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(LoginPageActivity.this)
+//                .setTitle(title)
+//                .setMessage(message)
+//                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+////                        Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
+////                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+////                        startActivity(intent);
+//                    }
+//                });
+//        AlertDialog ok = builder.create();
+//        ok.show();
+//    }
 
     public void createAccountBtnClicked(View view) {
 
         username = usernameEditTextSignup.getText().toString();
+        email = emailEditTextSignUp.getText().toString();
         password = passwordEditTextSignup.getText().toString();
 
-        ParseUser user = new ParseUser();
-        user.setUsername(username);
-        user.setPassword(username);
+        if (!emailValidator(email)) {
+            emailEditTextSignUp.setError("Please enter valid email");
+        }
+        else if (password.length() == 0) {
+            passwordEditTextSignup.setError("Password is required");
+        }
+        else if (password.length() < 8) {
+            passwordEditTextSignup.setError("Password length should be atleast 8 characters");
+        }
+
+        else {
+            ParseUser user = new ParseUser();
+            user.setUsername(username);
+            user.setEmail(email);
+            user.setPassword(password);
 
 
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    alertDisplayer("Sucessful Sign Up!","Welcome" + username + "!");
-                } else {
-                    ParseUser.logOut();
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+//                    alertDisplayer("Sucessful Sign Up!","Welcome " + username + "!");
+                        Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
+                        System.out.println("Signup finish");
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    } else {
+                        usernameEditTextSignup.setError("Please enter different username");
+                        System.out.println("Signup error");
+                        ParseUser.logOut();
 //                    Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
                 }
-            }
-        });
+            });
+        }
+    }
+
+    public boolean emailValidator(String email)
+    {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+
+
     }
 
     public void loginBtnClicked(View view) {
 
 
         username = usernameEditTextLogin.getText().toString();
+        System.out.println(username);
         password = passwordEditTextLogin.getText().toString();
-
+        System.out.println(password);
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
             public void done(ParseUser parseUser, ParseException e) {
+                System.out.println(parseUser);
                 if (parseUser != null) {
-                    alertDisplayer("Sucessful Login","Welcome back" + username + "!");
+                    Intent intent = new Intent(LoginPageActivity.this, MainActivity.class);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
                 } else {
                     ParseUser.logOut();
                     Toast.makeText(LoginPageActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
@@ -143,10 +185,5 @@ public class LoginPageActivity extends AppCompatActivity {
         });
     }
 
-    public void logoutBtnClicked(View view) {
 
-        ParseUser.logOut();
-        alertDisplayer("So, you're going...", "Ok...Bye-bye then");
-
-    }
 }
