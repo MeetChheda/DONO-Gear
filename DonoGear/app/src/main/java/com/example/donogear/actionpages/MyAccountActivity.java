93ee23f45df667a16aeb8fb0fc72de5e7ef2cf37
Ernest Legier <ernest.legier@bison.howard.edu>
2019-11-21 14:47:55 -0800

    DONOG-96: completed MyAccountActivity

--- DonoGear/app/src/main/java/com/example/donogear/actionpages/MyAccountActivity.java
+++ DonoGear/app/src/main/java/com/example/donogear/actionpages/MyAccountActivity.java
@@ -1,18 +1,29 @@
 package com.example.donogear.actionpages;
 
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.text.Layout;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.donogear.R;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.view.View.GONE;
 import static android.view.View.INVISIBLE;
+import static com.example.donogear.utils.Constants.EMAIL_PATTERN;
 
 public class MyAccountActivity extends AppCompatActivity {
     private static final String PHONE_NUM = "phoneNumber";
@@ -25,15 +36,11 @@ public class MyAccountActivity extends AppCompatActivity {
     private EditText userNameInput;
     private EditText emailInput;
     private EditText phoneNumberInput;
-    private EditText oldPasswordInput;
-    private EditText newPasswordInput;
     private Button updateUserSettingsButton;
-    private Button updatePasswordButton;
     private Button cancelSettingsUpdateButton;
-    private Button cancelPasswordUpdateButton;
     private TextView guestUserMessage;
     private boolean isUpdatingUserSettings = false;
-    private boolean isUpdatingPassword = false;
+    private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +48,6 @@ public class MyAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_account);
 
         updateUserSettingsButton = (Button) findViewById(R.id.update_settings_button);
-        updatePasswordButton = (Button) findViewById(R.id.change_password_button);
         initializeLayout();
 
         updateUserSettingsButton.setOnClickListener(new View.OnClickListener() {
@@ -52,24 +58,18 @@ public class MyAccountActivity extends AppCompatActivity {
                     enableUserSettingsLayout();
                     isUpdatingUserSettings = true;
                 } else {
-                    disableUserSettingsLayout();
-                    isUpdatingUserSettings = false;
+                    boolean isValidInputs = validateUserInputs();
+
+                    if (isValidInputs) {
+                        updateUserSettings();
+                        disableUserSettingsLayout();
+                        isUpdatingUserSettings = false;
+                    }
                 }
             }
         });
 
-        updatePasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (! isUpdatingPassword) {
-                    enablePasswordLayout();
-                    isUpdatingPassword = true;
-                } else {
-                    disablePasswordLayout();
-                    isUpdatingPassword = false;
-                }
-            }
-        });
+
     }
 
     private void initializeLayout() {
@@ -83,14 +83,36 @@ public class MyAccountActivity extends AppCompatActivity {
         }
     }
 
+    private boolean validateUserInputs() {
+        boolean isValid = true;
+
+        clearInputErrors();
+
+        if (TextUtils.isEmpty(userNameInput.getText())){
+            userNameInput.setError("User name is required");
+            isValid = false;
+        }
+
+        if (! pattern.matcher(emailInput.getText()).matches()) {
+            emailInput.setError("Please enter valid email");
+            isValid = false;
+        }
+
+        if (! TextUtils.isEmpty(phoneNumberInput.getText()) && ! PhoneNumberUtils.isGlobalPhoneNumber(phoneNumberInput.getText().toString())) {
+            phoneNumberInput.setError("Please enter a valid phone number");
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
     private void initializeAuthenticatedUserLayout(ParseUser user) {
-        currentUserName = user.getUsername();
-        currentEmail = user.getEmail();
-        currentPhoneNumber = (String) user.get(PHONE_NUM);
+        updateCurrentUserInfo(user);
 
         userNameInput = (EditText) findViewById(R.id.fullName);
         emailInput = (EditText) findViewById(R.id.emailAddress);
         phoneNumberInput = (EditText) findViewById(R.id.phoneNum);
+        cancelSettingsUpdateButton = (Button) findViewById(R.id.cancel_user_profile_update_button);
 
         userNameInput.setText(currentUserName);
         emailInput.setText(currentEmail);
@@ -100,11 +122,9 @@ public class MyAccountActivity extends AppCompatActivity {
     private void initializeGuestUserLayout() {
         guestUserMessage = (TextView) findViewById(R.id.guest_user_message);
         guestUserMessage.setVisibility(View.VISIBLE);
-//        userNameInput.setVisibility(INVISIBLE);
-//        emailInput.setVisibility(INVISIBLE);
-//        phoneNumberInput.setVisibility(INVISIBLE);
-//        updateUserSettingsButton.setVisibility(INVISIBLE);
-//        updatePasswordButton.setVisibility(INVISIBLE);
+
+        LinearLayout userSettings = (LinearLayout) findViewById(R.id.user_settings);
+        userSettings.setVisibility(GONE);
     }
 
     private void enableUserSettingsLayout() {
@@ -113,27 +133,58 @@ public class MyAccountActivity extends AppCompatActivity {
         emailInput.setEnabled(true);
         updateUserSettingsButton.setText("Save Settings");
         cancelSettingsUpdateButton.setVisibility(View.VISIBLE);
+
+        cancelSettingsUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userNameInput.setText(currentUserName);
+                emailInput.setText(currentEmail);
+                phoneNumberInput.setText(currentPhoneNumber);
+                clearInputErrors();
+                disableUserSettingsLayout();
+            }
+        });
     }
 
     private void disableUserSettingsLayout() {
-        userNameInput.setEnabled(true);
-        phoneNumberInput.setEnabled(true);
-        emailInput.setEnabled(true);
+        userNameInput.setEnabled(false);
+        phoneNumberInput.setEnabled(false);
+        emailInput.setEnabled(false);
         updateUserSettingsButton.setText("Update Settings");
         cancelSettingsUpdateButton.setVisibility(INVISIBLE);
     }
 
-    private void enablePasswordLayout() {
-        newPasswordInput.setEnabled(true);
-        oldPasswordInput.setEnabled(true);
-        updateUserSettingsButton.setText("Save Password");
-        cancelPasswordUpdateButton.setVisibility(View.VISIBLE);
+    private void clearInputErrors() {
+        userNameInput.setError(null);
+        emailInput.setError(null);
+        phoneNumberInput.setError(null);
     }
 
-    private void disablePasswordLayout() {
-        newPasswordInput.setEnabled(false);
-        oldPasswordInput.setEnabled(false);
-        updateUserSettingsButton.setText("Update Password");
-        cancelPasswordUpdateButton.setVisibility(INVISIBLE);
+    private void updateUserSettings() {
+        ParseUser user = ParseUser.getCurrentUser();
+
+        user.setUsername(userNameInput.getText().toString());
+        user.setEmail(emailInput.getText().toString());
+
+        String phoneNum = ! TextUtils.isEmpty(phoneNumberInput.getText()) ? phoneNumberInput.getText().toString().trim() : "";
+        user.put(PHONE_NUM, phoneNum);
+
+        try {
+            user.saveInBackground();
+            user = user.fetch();
+            updateCurrentUserInfo(user);
+        } catch (ParseException e) {
+            Log.e("MyAccountPage", "Unable to save user info:" + e.getMessage(), e);
+        }
+    }
+
+    private void updateCurrentUserInfo(ParseUser user) {
+        currentUserName = user.getUsername();
+        currentEmail = user.getEmail();
+        Object phoneNum = user.get(PHONE_NUM);
+
+        if (phoneNum != null) {
+            currentPhoneNumber = (String) phoneNum;
+        }
     }
 }
