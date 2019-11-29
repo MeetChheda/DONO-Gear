db9f70c9d825ac20e2e8fa03af9acc6a63d01ddb
Ernest Legier <ernest.legier@bison.howard.edu>
2019-11-29 11:37:17 -0600

    DONOG-125: added confirm and cancel buttons for payment

--- DonoGear/app/src/main/java/com/example/donogear/actionpages/PaymentInfoActivity.java
+++ DonoGear/app/src/main/java/com/example/donogear/actionpages/PaymentInfoActivity.java
@@ -1,6 +1,9 @@
 package com.example.donogear.actionpages;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,9 +23,27 @@ public class PaymentInfoActivity extends AppCompatActivity {
                 .cvvRequired(true)
                 .cardholderName(CardForm.FIELD_REQUIRED)
                 .postalCodeRequired(true)
-                .mobileNumberRequired(true)
-                .mobileNumberExplanation("SMS is required on this number")
-                .actionLabel("Purchase")
+                .actionLabel("Save")
                 .setup(this);
+
+        Button confirmBtn = findViewById(R.id.confirm_btn);
+        confirmBtn.setOnClickListener(v -> {
+            if (cardForm.isValid()) {
+                Intent intent = new Intent(this, MyAccountActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Must fill all required fields",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button cancelBtn = findViewById(R.id.cancel_payment_update_btn);
+        cancelBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MyAccountActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
     }
 }
